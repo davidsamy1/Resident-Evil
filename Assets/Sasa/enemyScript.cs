354d3420626c67c0f226c0914096d075e615633f
MostafaAshraf11 <mostafa472001@gmail.com>
2023-12-12 00:46:14 +0200

    zombie animation controller

---
+++ Assets/Sasa/enemyScript.cs
@@ -0,0 +1,150 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor.AI;
+using UnityEngine.AI;
+using StarterAssets;
+using System;
+
+public class enemyScript : MonoBehaviour
+{
+    
+    public Transform player;
+    private NavMeshAgent agent;
+    Animator animator;
+
+
+    public float maxTime = 1.0f;
+    public float maxDistance = 1.0f;
+    float timer = 0.0f;
+    // Start is called before the first frame update
+
+    private TPSController TPSController;
+
+    public float currentHealth = 5;
+
+    private bool isDead= false;
+    private bool isHit = false;
+    private bool isKnockedDown = false;
+    private bool isAttack = false;
+
+    public float attackRange;
+    void Start()
+    {
+        TPSController = GetComponent<TPSController>();
+        //BulletCollider;
+        agent = GetComponent<NavMeshAgent>();
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isDead || isKnockedDown)
+        {
+            agent.isStopped = true;
+            return;
+        }
+
+        timer = (timer - Time.deltaTime);
+
+        float distanceToPlayer = Vector3.Distance(transform.position, player.position);
+
+        // Check if the enemy is close to the player to initiate an attack
+        if (distanceToPlayer < attackRange && isAttack==false)
+        {
+            Attack();
+        }
+
+        if (timer < 0.0f)
+        {
+            float distance = (player.transform.position - agent.destination).sqrMagnitude;
+            if(distance > maxDistance*maxDistance)
+            {
+                agent.destination = player.position;
+
+            }
+            timer = maxTime;
+        }
+        animator.SetFloat("Speed", agent.velocity.magnitude);
+
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            TakeDamage(1);
+        }
+
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            KnockdeDown();
+        }
+
+    }
+
+    public void TakeDamage(float damage)
+    {
+        currentHealth -= damage;
+        if (currentHealth <= 0)
+        {
+            Die();
+        }
+        else
+        {
+            animator.SetTrigger("TakeDamage");
+            isHit = true;
+            agent.isStopped = true;
+            Invoke("ResumeWalking", 1.5f); // Adjust the delay as needed
+        }
+    }
+
+    private void Die()
+    {
+        animator.SetTrigger("Die");
+        isDead=true;
+    }
+
+    private void ResumeWalking()
+    {
+        if (!isDead)
+        {
+            // Resume the NavMeshAgent and reset the hit flag
+            agent.isStopped = false;
+            isHit = false;
+            isKnockedDown = false;
+            isAttack = false;
+        }
+    }
+
+    private void GetUp()
+    {
+        if (!isDead) { 
+            animator.SetTrigger("GetUp");
+            agent.isStopped = true;
+            Invoke("ResumeWalking", 2.0f); // Adjust the delay as needed
+        }
+    }
+
+
+    private void KnockdeDown()
+    {
+        if (!isDead)
+        {
+            animator.SetTrigger("KnockedDown");
+            agent.isStopped = true;
+            Invoke("GetUp", 4.0f); // Adjust the delay as needed
+            isKnockedDown = true;
+        }
+    }
+
+    private void Attack()
+    {
+        if (!isDead)
+        {
+            animator.SetTrigger("Attack");
+            agent.isStopped = true;
+            isAttack = true;
+            Invoke("ResumeWalking", 1.5f); // Adjust the delay as needed
+        }
+    }
+
+
+}
